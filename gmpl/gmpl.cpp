5a49a212f3da82791ab555d5098b61cfc61da92b
Leslie Leigh <leslieleigh@outlook.com>
2022-03-04 19:44:46 +0800

    Initial commit

---
+++ gmpl/gmpl.cpp
@@ -0,0 +1,47 @@
+﻿#include "gmpl.h"
+#include <iostream>
+#include <vector>
+
+class Vec3 {
+public:
+  using value_type = double;
+
+  value_type x, y, z;
+};
+
+class Vertex {
+
+};
+
+class Polygon {
+public:
+  using vertex_type = Vertex;
+  using vertex_count_type = std::size_t;
+
+  Vertex vertex(vertex_count_type index_) {
+    return _vertices[index_];
+  }
+
+private:
+  std::vector<Vertex> _vertices;
+};
+
+class Mesh {
+public:
+  using polygon_type = Polygon;
+  using polygon_count_type = std::size_t;
+
+  Polygon polygon(polygon_count_type index_) {
+    return _polygons[index_];
+  }
+
+private:
+  std::vector<Polygon> _polygons;
+};
+
+int main() {
+  Mesh mesh;
+  gmpl::triangluate(mesh);
+  std::cout << "Hello CMake." << std::endl;
+  return 0;
+}
---
+++ gmpl/gmpl.h
@@ -0,0 +1,48 @@
+﻿
+#pragma once
+
+#include <concepts>
+
+namespace gmpl {
+template <typename T>
+concept Vec3 = requires(T a) {
+  typename T::value_type;
+  { a.x } -> std::same_as<typename T::value_type>;
+  { a.y } -> std::same_as<typename T::value_type>;
+  { a.z } -> std::same_as<typename T::value_type>;
+};
+
+template <typename T>
+concept Vertex = requires(T a) {
+  { a.position } -> Vec3;
+};
+
+template <typename T>
+concept Polygon = requires(T a, typename T::vertex_count_type vertex_index_) {
+  typename T::vertex_count_type;
+  typename T::vertex;
+  { a.vertex_count() } -> std::same_as<typename T::vertex_count_type>;
+  { a.vertex(vertex_index_) } -> std::same_as<typename T::vertex>;
+};
+
+template <typename T>
+concept Mesh = requires(T a, typename T::polygon_count_type polygon_index_) {
+  typename T::polygon_count_type;
+  typename T::polygon_type;
+  { a.polygon_count() } -> std::same_as<typename T::polygon_count_type>;
+  { a.polygon(polygon_index_) } -> std::same_as<typename T::polygon_type>;
+};
+
+template <Mesh TMesh> void triangluate(TMesh &mesh_) {
+  const auto nPolygons = mesh_.polygon_count();
+  for (typename TMesh::polygon_count_type iPolygon = 0; iPolygon < nPolygons;
+       ++iPolygon) {
+    const auto polygon = mesh_.polygon(iPolygon);
+    const auto nPolygonVertices = polygon.vertex_count();
+    for (typename TMesh::polygon_type::vertex_count_type iPolygonVertex = 0;
+         iPolygonVertex < nPolygonVertices; ++iPolygonVertex) {
+      const auto vertex = polygon.vertex(iPolygonVertex);
+    }
+  }
+}
+} // namespace gmpl
